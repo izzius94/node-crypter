8f77ec6128c4adae1d890a8db2620095cd44b633
Izzius1994 <94847018+Izzius1994@users.noreply.github.com>
2023-07-30 15:11:23 +0200

    refactor: moved crypter logic to crypt module

--- src/lib/crypter.ts
+++ src/lib/crypter.ts
@@ -1,4 +1,4 @@
-import { Hmac, createCipheriv, createDecipheriv, createHmac, randomBytes, timingSafeEqual } from 'crypto'
+import { decrypt, encrypt } from './crypt'
 
 export default class {
   protected readonly key: Buffer
@@ -12,40 +12,20 @@ export default class {
    * Method to decrypt an encrypted string
    *
    * @param encrypted The string to decrypt
-   * @param key The key to be used to encrypt the string, if not provided the default key is used
    * @returns The string decrypted
    */
-  public decrypt (encrypted: string, key: Buffer = this.key): string {
-    this.checkKey(key)
-    const data = this.getJsonPayload(encrypted, key)
-    const decipher = createDecipheriv('AES-256-CBC', Buffer.from(key), Buffer.from(data.iv, 'base64'))
-    const decrypted = decipher.update(data.value, 'base64')
-
-    return Buffer.concat([decrypted, decipher.final()]).toString()
+  public decrypt (encrypted: string): string {
+    return decrypt(encrypted, this.key)
   }
 
   /**
    * Method to enctypt a string
    *
    * @param value The value to encrypt
-   * @param key The key to be used to encrypt the string, if not provided the default key is used
    * @returns The string encrypted
    */
-  public encrypt (value: string, key: Buffer = this.key): string {
-    this.checkKey(key)
-    const iv = randomBytes(16)
-    const ivBased = iv.toString('base64')
-    const cipher = createCipheriv('AES-256-CBC', Buffer.from(key), iv)
-    const encrypted = cipher.update(value)
-    const value64 = Buffer.concat([encrypted, cipher.final()]).toString('base64')
-
-    return Buffer.from(
-      JSON.stringify({
-        iv: ivBased,
-        value: value64,
-        mac: this.hash(ivBased, value64, key).digest('hex')
-      })
-    ).toString('base64')
+  public encrypt (value: string): string {
+    return encrypt(value, this.key)
   }
 
   /**
@@ -58,73 +38,4 @@ export default class {
       throw Error('Invalid key lenght.')
     }
   }
-
-  /**
-   * Get the JSON object from the given payload.
-   *
-   * @param encrypted The payload to convert
-   * @param key The key used for the encryption
-   * @returns The converted payload
-   */
-  protected getJsonPayload (encrypted: string, key: Buffer): IPayload {
-    const data: IPayload = JSON.parse(Buffer.from(encrypted, 'base64').toString('utf8'))
-
-    if (!this.validJson(data)) {
-      throw new Error('Invalid payload.')
-    }
-
-    if (!this.validMac(data, key)) {
-      throw new Error('Invalid MAC.')
-    }
-
-    return data
-  }
-
-  /**
-   * Check if the given paylod is valid
-   *
-   * @param payload The payload to check
-   * @returns The validity of the payload
-   */
-  protected validJson (payload: IPayload): boolean {
-    return typeof payload === 'object' && (payload.iv !== '' && payload.value !== '' && payload.mac !== '') && Buffer.from(payload.iv, 'base64').toString('ascii').length === 16
-  }
-
-  /**
-   * Determine if the MAC for the given payload is valid.
-   *
-   * @param payload The payload to check
-   * @param key The key used for the encryption
-   * @returns The validity of the MAC
-   */
-  protected validMac (payload: IPayload, key: Buffer): boolean {
-    return timingSafeEqual(
-      Buffer.from(this.hash(payload.iv, payload.value, key).digest('base64'), 'base64'),
-      Buffer.from(payload.mac, 'hex')
-    )
-  }
-
-  /**
-   * Create a MAC for the given value
-   *
-   * @param iv The iv used to create the hash
-   * @param value The value used to create the hash
-   * @param key The key used to create the hash
-   * @returns The hash
-   */
-  protected hash (iv: string, value: string, key: Buffer): Hmac {
-    const hmac = createHmac('sha256', key)
-    hmac.update(iv + value)
-
-    return hmac
-  }
-}
-
-/**
- * Describe the structure of a payload
- */
-interface IPayload {
-  iv: string
-  value: string
-  mac: string
 }
--- test/lib/crypter.ts
+++ test/lib/crypter.ts
@@ -2,6 +2,10 @@ import Crypter from '../../src/lib/crypter'
 import { randomBytes } from 'crypto'
 import { expect } from 'chai'
 import { read } from '../../src/lib/key'
+import { key, encrypted, encryptedInvalidPayload, anotherKey } from '../config'
+
+
+const crypter = new Crypter(key)
 
 describe('Crypter', () => {
   describe('Initialization', () => {
@@ -10,29 +14,22 @@ describe('Crypter', () => {
     })
   })
   describe('Decrypt', () => {
-    const encrypted = 'eyJpdiI6IlRWaDZIZCtiNHJQMFJTNnhtOEJ4dkE9PSIsInZhbHVlIjoiYVlGL0RFWHhBR0N5Z011RjU0T2gwQT09IiwibWFjIjoiYzM1NWY0MjE2NDc0ZWM4ZjExNDRmYzRiYWFhOWZkYmJhY2VkNDEyZWY5NTc0Yjg0N2VjNTZhNTlmNGMwZTZhNiIsInRhZyI6IiJ9='
     it('Should decrypt a string', () => {
-      const crypter = new Crypter(read('1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw='))
-
       expect('test-string').equal(crypter.decrypt(encrypted))
     })
 
     it('Should rise an exception if the payload is invalid', () => {
-      const crypter = new Crypter(read('1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw'))
-
-      expect(() => crypter.decrypt('eyJpdiI6IiIsInZhbHVlIjoiQm1DZDMrQWdxNEtrZ2Zta3I3WGFxUT09IiwibWFjIjoiOTFkOTMyZWEzZGM3NWFmZTI1NWI2OTE0YzVmN2RkMjk4NjI3MTY1OGQ5NTJhODc5ODgyNGZiODA2MDZlM2JiNCJ9'))
-        .Throw('Invalid payload.')
+      expect(() => crypter.decrypt(encryptedInvalidPayload)).Throw('Invalid payload.')
     })
 
     it('Should rise an exception if the MAC is invalid', () => {
-      const crypter = new Crypter(read('8U6GU1Tp1/0Jb7/1BRCxpzQubzBKfs1Sm8V8Wtce4+U=='))
+      const crypter = new Crypter(anotherKey)
 
       expect(() => crypter.decrypt(encrypted)).Throw('Invalid MAC.')
     })
   })
   describe('Encrypt', () => {
     it('Should encrypt a string correctly', () => {
-      const crypter = new Crypter(read('1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw='))
       const original = 'test-string'
       const encrypted = crypter.encrypt(original)
       const decrypted = crypter.decrypt(encrypted)
