96d7819916d67f43b9c6544005feb087f2da4b6b
Izzius1994 <94847018+Izzius1994@users.noreply.github.com>
2023-07-31 14:36:25 +0200

    test: improved tests syntax

--- test/lib/key.ts
+++ test/lib/key.ts
@@ -1,36 +1,17 @@
 import assert from 'assert'
 import { generate, read, algo } from '../../src/lib/key'
+import forEach from 'mocha-each'
 
-const keys: {algo: algo, size: number, key: string, length: number}[] = [{
-  algo: 'aes-256-cbc',
-  size: 44,
-  key: '1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw=',
-  length: 32
-}, {
-  algo: 'aes-128-cbc',
-  size: 24,
-  key: 'OT8by/782/nqNUwrm4TbLg==',
-  length: 16
-}] 
-
-keys.forEach(element => {
-  describe(`Key module with algo ${element.algo}`, () => {
-    describe('# Generation', () => {
-      it(`Should create an encryption key string of ${element.size} characters`, () => {
-        const key = generate(element.algo)
-
-        assert.strictEqual(key.length, element.size)
-      })
-      it('Should create different keys every time', () => {
-        assert.notEqual(generate(), generate())
-      })
+describe('Key module', () => {
+  forEach([
+    ['aes-256-cbc', 44, 32, '1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw='],
+    ['aes-128-cbc', 24, 16, 'OT8by/782/nqNUwrm4TbLg==']
+  ]).describe('# Algo %s', (algo: algo, size: number, length: number, key: string) => {
+    it(`Should create an encryption key string of ${size} characters`, () => {
+      assert.strictEqual(generate(algo).length, size)
     })
-    describe('# Reading', () => {
-      it('Could read an encryption key created by a laravel project', () => {
-        const key = read(element.key)
-
-        assert.strictEqual(key.length, element.length)
-      })
+    it('Could read an encryption key created by a laravel project', () => {
+      assert.strictEqual(read(key).length, length)
     })
   })
 })
