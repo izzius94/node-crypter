56191af7d7816e1642b6c13b52da2b8432f9de58
Izzius1994 <94847018+Izzius1994@users.noreply.github.com>
2023-07-31 13:12:35 +0200

    feat: added support to generate aes-128-cbc keys

--- test/lib/key.ts
+++ test/lib/key.ts
@@ -1,22 +1,36 @@
 import assert from 'assert'
-import { generate, read } from '../../src/lib/key'
+import { generate, read, algo } from '../../src/lib/key'
 
-describe('Key module', () => {
-  describe('# Generation', () => {
-    it('Should create an encryption key string of 44 characters', () => {
-      const key = generate()
+const keys: {algo: algo, size: number, key: string, length: number}[] = [{
+  algo: 'aes-256-cbc',
+  size: 44,
+  key: '1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw=',
+  length: 32
+}, {
+  algo: 'aes-128-cbc',
+  size: 24,
+  key: 'OT8by/782/nqNUwrm4TbLg==',
+  length: 16
+}] 
 
-      assert.strictEqual(key.length, 44)
+keys.forEach(element => {
+  describe(`Key module with algo ${element.algo}`, () => {
+    describe('# Generation', () => {
+      it(`Should create an encryption key string of ${element.size} characters`, () => {
+        const key = generate(element.algo)
+
+        assert.strictEqual(key.length, element.size)
+      })
+      it('Should create different keys every time', () => {
+        assert.notEqual(generate(), generate())
+      })
     })
-    it('Should create different keys every time', () => {
-      assert.notEqual(generate(), generate)
-    })
-  })
-  describe('# Reading', () => {
-    it('Could read an encryption key created by a laravel project', () => {
-      const key = read('1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw=')
+    describe('# Reading', () => {
+      it('Could read an encryption key created by a laravel project', () => {
+        const key = read(element.key)
 
-      assert.strictEqual(key.length, 32)
+        assert.strictEqual(key.length, element.length)
+      })
     })
   })
 })
