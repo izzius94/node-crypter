aa2d3becbd16cabcb2413f1d1f908edc2d39784c
Izzius1994 <94847018+Izzius1994@users.noreply.github.com>
2023-08-02 23:43:53 +0200

    test: added test support for aes-128-gcm

--- test/lib/crypt.ts
+++ test/lib/crypt.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai'
 import { decrypt, encrypt } from '../../src/lib/crypt'
-import { encryptedInvalidPayload, anotherKey } from '../config'
+import { encryptedInvalidPayload } from '../config'
 import forEach from 'mocha-each'
 import { algo, read } from '../../src/lib/key'
 
 describe('crypt', () => {
   forEach([
-    ['aes-256-cbc', '1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw=', 'eyJpdiI6IlRWaDZIZCtiNHJQMFJTNnhtOEJ4dkE9PSIsInZhbHVlIjoiYVlGL0RFWHhBR0N5Z011RjU0T2gwQT09IiwibWFjIjoiYzM1NWY0MjE2NDc0ZWM4ZjExNDRmYzRiYWFhOWZkYmJhY2VkNDEyZWY5NTc0Yjg0N2VjNTZhNTlmNGMwZTZhNiIsInRhZyI6IiJ9'],
-    ['aes-128-cbc', 'OT8by/782/nqNUwrm4TbLg==', 'eyJpdiI6IkZRZVVZYmRsNDJDQkJpQ2hqZXEwSkE9PSIsInZhbHVlIjoiQWhZT0dBVi9vWC8xTndHMGJaZ3YxUT09IiwibWFjIjoiYzY5Y2Y4YmY2ODNjMThmOTU3NTBkNDkyNzNkNGZmYjhlZDNlMzk2MTAzZjEyMDEwNDBjODYxYjMyYTRlY2E5YyIsInRhZyI6IiJ9'],
+    ['aes-256-cbc', '1OvhreDcn1XxUcu8Pj1OgY7IIAD9cQzKB2vdu2YRLrw=', 'eyJpdiI6IlRWaDZIZCtiNHJQMFJTNnhtOEJ4dkE9PSIsInZhbHVlIjoiYVlGL0RFWHhBR0N5Z011RjU0T2gwQT09IiwibWFjIjoiYzM1NWY0MjE2NDc0ZWM4ZjExNDRmYzRiYWFhOWZkYmJhY2VkNDEyZWY5NTc0Yjg0N2VjNTZhNTlmNGMwZTZhNiIsInRhZyI6IiJ9', '8U6GU1Tp1/0Jb7/1BRCxpzQubzBKfs1Sm8V8Wtce4+U=='],
+    ['aes-128-cbc', 'OT8by/782/nqNUwrm4TbLg==', 'eyJpdiI6IkZRZVVZYmRsNDJDQkJpQ2hqZXEwSkE9PSIsInZhbHVlIjoiQWhZT0dBVi9vWC8xTndHMGJaZ3YxUT09IiwibWFjIjoiYzY5Y2Y4YmY2ODNjMThmOTU3NTBkNDkyNzNkNGZmYjhlZDNlMzk2MTAzZjEyMDEwNDBjODYxYjMyYTRlY2E5YyIsInRhZyI6IiJ9', 'bWfLYaiK0PHaJtyp3NnXfg=='],
     ['aes-256-gcm', 'sCzNv3C9UhFcplY+4neT+ALCQLq+IQwkbCYAnxIFAHQ=', 'eyJpdiI6IkV5NTRIOGdaek1ZS2hsRXMiLCJ2YWx1ZSI6ImZDMThqa1RyS1dkS0lGTT0iLCJtYWMiOiIiLCJ0YWciOiJ0U1FLUnNCaHQwTUJaUDVsK0RhYnJnPT0ifQ==', 'puQm02xCG7krF8zfr0vb6sgFzfYd1w3zrdjGXVpQTV8='],
     ['aes-128-gcm', 'bWfLYaiK0PHaJtyp3NnXfg==', 'eyJpdiI6IjM3TTd2VXNDMFBmNjB3eGsiLCJ2YWx1ZSI6IlB1ZGhTbFE1bzBVc005VT0iLCJtYWMiOiIiLCJ0YWciOiJCMlkxd2NmMDZSZmcrV3ZyWVlRQnlBPT0ifQ==', 'ZBq7Bx+oG0GVd8ixu4Y+mQ==']
   ]).describe('Using %s', (algo: algo, key: string, encrypted: string, otherKey: string) => {
@@ -17,7 +17,7 @@ describe('crypt', () => {
       })
       it('Should rise an exception if the MAC is invalid', () => {
         if (algo === 'aes-128-cbc' || algo === 'aes-256-cbc') {
-          return expect(() => decrypt(encrypted, anotherKey, algo), algo).Throw('Invalid MAC.')
+          return expect(() => decrypt(encrypted, read(otherKey), algo), algo).Throw('Invalid MAC.')
         }
 
         return expect(() => decrypt(encrypted, read(otherKey), algo), algo).Throw('Unsupported state or unable to authenticate data')
